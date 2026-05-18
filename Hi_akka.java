61cbea9657f61b38bffbb6e859a22a6a16486bb6
Likith Naidu <likith.anumakonda@gmail.com>
2026-05-18 18:43:04 +0530

    solve cheyyandi akka koncham

---
+++ Hi_akka.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Hi_akka{
+   public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("ENTER SIZE BABU");
+    int a=sc.nextInt();
+    for(int i=0;i<a;i++){
+     for(int j=0;j<a;j++){
+          if(i==0 || i==a-1 || j==0 || j==a-1){
+       System.out.println("*/n");
+            }
+         }
+       System.out.println();
+      }
+   }
+}
